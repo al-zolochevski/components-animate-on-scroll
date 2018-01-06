30da69473d8159cbfb5646d23e83532ce1d6d6d1
Aleksey Zolochevskiy <aleksey.zolochevski@gmail.com>
2018-01-06 12:35:25 +0200

    + aos-fade-down (mod)

--- demo.js
+++ demo.js
@@ -10,6 +10,21 @@ var showElementsOnScreen =  function () {
     })
 };
 
+var showElementsOnScreen1 =  function () {
+    // TODO: z('.aos-fade').forEach(...)
+    document.querySelectorAll(".aos-fade-down").forEach(function (element) {
+        var zElement = z(element);
+        if (zElement.isOnScreen()) {
+            zElement.addClass("aos-animate")
+        } else {
+            zElement.removeClass("aos-animate")
+        }
+    })
+};
 showElementsOnScreen();
+showElementsOnScreen1();
+
 z(window).on("scroll", showElementsOnScreen);
+
+z(window).on("scroll", showElementsOnScreen1);
 
