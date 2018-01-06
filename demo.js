8487a04da81a3c800b26492f3cfed498967a3d83
Aleksey Zolochevskiy <aleksey.zolochevski@gmail.com>
2018-01-06 12:17:13 +0200

    + just .aos-fade-up

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 var showElementsOnScreen =  function () {
     // TODO: z('.aos-fade').forEach(...)
-    document.querySelectorAll(".aos-fade").forEach(function (element) {
+    document.querySelectorAll(".aos-fade-up").forEach(function (element) {
         var zElement = z(element);
         if (zElement.isOnScreen()) {
             zElement.addClass("aos-animate")
